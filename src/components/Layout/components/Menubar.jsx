694a9613cf1b37be754f709b14fca140d9e30ee0
unknown <gmyhy0510@naver.com>
2023-03-29 14:25:45 +0900

    menu?

--- src/components/Layout/components/Menubar.jsx
+++ src/components/Layout/components/Menubar.jsx
@@ -1,23 +1,34 @@
 import style from "@/styles/Home.module.css";
 import { useRouter } from "next/router";
+import { Menu } from "css.gg/index";
 
 const Menubar = () => {
   const route = useRouter();
   const menu_title = [
     "Home",
+    "스토리",
     "그림",
     "음악",
+    "영상",
     "PPT",
-    "스토리",
     "프로그램",
-    "Menu6",
     "Menu7",
   ];
 
   return (
-    <div className={style.layout_left_menu}>
+    <div className={style.layout_left_menu_wrap}>
       {menu_title.map((val, index) => {
-        return <div className={style.layout_left_userName_menu}>{val}</div>;
+        return (
+          <div className={style.layout_left_menu}>
+            <div className={style.layout_left_menu_icon}>
+              {/* <svg width="24" height="24">
+                <use xlinkHref={SVG + "#gg-home"} />
+              </svg> */}
+              <Menu />
+            </div>
+            <div className={style.layout_left_menu_title}>{val}</div>
+          </div>
+        );
       })}
     </div>
   );
