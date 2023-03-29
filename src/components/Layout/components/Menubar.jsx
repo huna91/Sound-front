6d92342199f1a99ad3b7ea3edbf0718045ee503d
huna91 <gmyhy0510@naver.com>
2023-03-29 18:59:20 +0900

    d

--- src/components/Layout/components/Menubar.jsx
+++ src/components/Layout/components/Menubar.jsx
@@ -1,9 +1,10 @@
 import style from "@/styles/Home.module.css";
 import { useRouter } from "next/router";
-import { Menu } from "css.gg/index";
+import { useState } from "react";
 
 const Menubar = () => {
   const route = useRouter();
+  const [curMenu, setCurMenu] = useState(0);
   const menu_title = [
     "Home",
     "스토리",
@@ -15,17 +16,22 @@ const Menubar = () => {
     "Menu7",
   ];
 
+  const navi = (_index) => {
+    setCurMenu(_index);
+    console.log(_index);
+  };
+
   return (
     <div className={style.layout_left_menu_wrap}>
       {menu_title.map((val, index) => {
         return (
-          <div className={style.layout_left_menu}>
-            <div className={style.layout_left_menu_icon}>
-              {/* <svg width="24" height="24">
-                <use xlinkHref={SVG + "#gg-home"} />
-              </svg> */}
-              <Menu />
-            </div>
+          <div
+            className={style.layout_left_menu}
+            onClick={() => {
+              navi(index);
+            }}
+          >
+            <div className={style.layout_left_menu_icon}></div>
             <div className={style.layout_left_menu_title}>{val}</div>
           </div>
         );
